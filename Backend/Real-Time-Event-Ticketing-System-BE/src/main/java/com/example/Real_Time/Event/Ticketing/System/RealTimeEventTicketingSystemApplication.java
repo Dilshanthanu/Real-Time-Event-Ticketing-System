756c9c60bf818538799d4a842485c335ea7ea05c
dilshan.t <dilshan.t@informaticsint.com>
2024-10-30 15:53:10 +0530

    Initial commit

---
+++ Backend/Real-Time-Event-Ticketing-System-BE/src/main/java/com/example/Real_Time/Event/Ticketing/System/RealTimeEventTicketingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.Real_Time.Event.Ticketing.System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealTimeEventTicketingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealTimeEventTicketingSystemApplication.class, args);
+	}
+
+}
